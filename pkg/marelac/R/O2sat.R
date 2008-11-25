070324a4881760e12dc359361a9edecfaaefdc07
petzoldt (Dr ) <thomas.petzoldt@tu-dresden.de>
2008-11-25 21:06:38 +0000

    several fixes in thpe's part
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/marelac@11 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/marelac/R/O2sat.R
+++ pkg/marelac/R/O2sat.R
@@ -1,10 +1,9 @@
-`O2sat` <-
-function(S=0, T = K - 273.15, K = T+273.15, NN = 0, method=c("APHA", "Weiss", "Paul")) {
+O2sat <- function(S=0, T = K - 273.15, K = T+273.15, NN = 0, method=c("Weiss", "APHA", "Paul")) {
    log10 <- function(x) log(x)/log(10)
    method <- match.arg(method)
    if ((S !=0) & (method != "Weiss")) warning("Salinity value ignored by this method!")
    if ((NN !=0) & (method != "Paul")) warning("Sea level height ignored by this method!")
-   switch(method,
+   ret <- switch(method,
      # American Public Health Association
      APHA  = exp(-139.34411 + (157570.1/K) - (66423080/K^2) + (12438000000/K^3)-
              (862194900000/K^4)),
@@ -16,5 +15,7 @@ function(S=0, T = K - 273.15, K = T+273.15, NN = 0, method=c("APHA", "Weiss", "P
      # Paul, L. approximation that respects height above see level
      Paul = (1012-0.12 * NN)/1013 * (14.674 - 13.644 * log10(1 + K/12.8))
    )
+   attr(ret, "unit") = "(g/m3)"
+   ret
 }
 

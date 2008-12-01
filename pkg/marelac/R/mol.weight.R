e5cf79c22fb7ed34f73150e5969c015ae80fb0d2
petzoldt (Dr ) <thomas.petzoldt@tu-dresden.de>
2008-12-01 22:50:56 +0000

    mol.weight2 --> mol.weight is now standard; fixed one capitalization issue in g2mol.Rd and marelac.rnw
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/marelac@22 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/marelac/R/mol.weight.R
+++ pkg/marelac/R/mol.weight.R
@@ -1,5 +1,47 @@
+##########################################################################
+# molar weight
+##########################################################################
+
+## ToDo: use the list of common species for speed-up
+
+#mol.weight <- function(species)
+## converts from mol to g
+#{
+#  SP <- toupper(species)
+#  switch(SP,
+#         C     = AtomicWeight$C,
+#         O     = AtomicWeight$O,
+#         H     = AtomicWeight$H,
+#         N     = AtomicWeight$N,
+#         S     = AtomicWeight$S,
+#         P     = AtomicWeight$P,
+#         SI    = AtomicWeight$Si,
+#         B     = AtomicWeight$B,
+#         CO2   = AtomicWeight$C+2*AtomicWeight$O,
+#         CO3   = AtomicWeight$C+3*AtomicWeight$O,
+#         HCO3  = AtomicWeight$H+AtomicWeight$C+3*AtomicWeight$O,
+#         NO3   = AtomicWeight$N+3*AtomicWeight$O,
+#         NH3   = AtomicWeight$N+3*AtomicWeight$H,
+#         NH4   = AtomicWeight$N+4*AtomicWeight$H,
+#         HS    = AtomicWeight$S+AtomicWeight$H,
+#         SO4   = AtomicWeight$S+4*AtomicWeight$O,
+#         HSO4  = AtomicWeight$H+AtomicWeight$S+4*AtomicWeight$O,
+#         PO4   = AtomicWeight$P+4*AtomicWeight$O,
+#         HPO4  = AtomicWeight$H+AtomicWeight$P+4*AtomicWeight$O,
+#         H2PO4 = 2*AtomicWeight$H+AtomicWeight$P+4*AtomicWeight$O,
+#         H3PO4 = 3*AtomicWeight$H+AtomicWeight$P+4*AtomicWeight$O,
+#         SIOH4 = AtomicWeight$Si+AtomicWeight$O+4*AtomicWeight$H,
+#         SIO2  = AtomicWeight$Si+2*AtomicWeight$O,
+#         BOH3  = AtomicWeight$B+AtomicWeight$O+3*AtomicWeight$H,
+#         NA     )
+#}
+
+##########################################################################
 # converts from mol to g, works with vectors and simple lists
-mol.weight2 <- function(species) {
+# general parser for arbitrary species
+# chemical species: whose molecular weight should be estimated
+##########################################################################
+mol.weight <- function(species) {
   if (!is.vector(species)) stop("species must be a vector")
   if (!is.character(unlist(species))) stop("species must be character")
   molweight <- function(species) {
@@ -30,3 +72,6 @@ mol.weight2 <- function(species) {
   sapply(species, molweight)
 }
 
+## chemical species whose weight must be converted to mol
+g2mol <- function(species)
+   1/mol.weight(species)

2377a78f27412c0b1496cff1752c9047bf076733
petzoldt (Dr ) <thomas.petzoldt@tu-dresden.de>
2008-12-09 22:28:17 +0000

    Conversions: Waals-Parameters as data set in /data/waalscoef.R
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/marelac@29 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/marelac/R/Conversions.r
+++ pkg/marelac/R/Conversions.r
@@ -1,22 +1,19 @@
 ##########################################################################
 # molar volume of ideal gas
 ##########################################################################
-
-mol.vol <- function(T=25,     # temperature, dg celsius
-                    P=1)      # pressure, atmosphere
-
+mol.vol <- function(T = 25,     # temperature, dg celsius
+                    P = 1       # pressure, atmosphere
+                   )
 {
-#  T in degrees Kelvin
-T = 273.15 + T
-R = 0.082058  # liter-atm / mole-K
-
-return(R* T / P)  # molar volume of an ideal gas
+  #  T in degrees Kelvin
+  TK <- 273.15 + T
+  R  <- 0.082058  # liter-atm / mole-K
+  return(R * TK / P)  # molar volume of an ideal gas
 }
 
 ##########################################################################
 # mol to liter and liter to mol conversion for a gas
 ##########################################################################
-
 mol2l <- function(x=1,      # mol of the gas
                   T=25,     # temperature, dg celsius
                   P=1,      # pressure, atmosphere
@@ -25,41 +22,55 @@ mol2l <- function(x=1,      # mol of the gas
                   b=0)      # dm3/mol
 # the van der Waals coefficients...
 {
-if (! is.null(gas))
-{
-Waals <- data.frame(
-a=c(  1.363,3.640,11.77,1.505,19.7483,6.579,13.04,12.18,
-  20.19,4.692,2.283,9.649,19.26,8.779,5.536,0.03457,
-  0.2476,4.510,3.716,4.490,8.200,2.349,4.225,0.2135,
-  1.358,1.408,5.354,3.832,1.378,4.692,4.377,4.251,6.803,4.250),
-b=c( 0.03219,0.04267,0.07685,0.03985,0.1281, 0.05622,0.09213,0.08407,
-  0.1286, 0.05264,0.04278,0.06702,0.146,  0.08445,0.03049,0.0237,
-  0.02661,0.04431,0.04081,0.04287,0.01696,0.03978,0.03707,0.01709,
-  0.02789,0.03913,0.04424,0.04415,0.03183,0.05156,0.05786,0.05571,
-  0.05636,0.05105))
-rownames(Waals)<-c("Ar","CO2","CS2","CO","CCl4","Cl2","C2H6S","C2H5OH",
-  "C6H5F","CH3F","CH4","CH3OH","C5H12","C3H8","H2O","He",
-  "H2","HBr","HCl","H2S","Hg","Kr","NH3","Ne",
-  "NO","N2","NO2","N2O","O2","PH3","SiH4","SiF4","SO2","Xe")
-  
- a <- Waals[gas,"a"]
- b <- Waals[gas,"b"]
- if (is.na(a)) stop(paste("do not have a and b values for the gas",gas))
-}
+  if (! is.null(gas))
+  {
+    ## simple indexed vectors are 5 times faster than row name indexing in data frame
+    #Waals.a <- c(  1.363,3.640,11.77,1.505,19.7483,6.579,13.04,12.18,
+    #  20.19,4.692,2.283,9.649,19.26,8.779,5.536,0.03457,
+    #  0.2476,4.510,3.716,4.490,8.200,2.349,4.225,0.2135,
+    #  1.358,1.408,5.354,3.832,1.378,4.692,4.377,4.251,6.803,4.250)
+    #
+    #Waals.b <- c( 0.03219,0.04267,0.07685,0.03985,0.1281, 0.05622,0.09213,0.08407,
+    #  0.1286, 0.05264,0.04278,0.06702,0.146,  0.08445,0.03049,0.0237,
+    #  0.02661,0.04431,0.04081,0.04287,0.01696,0.03978,0.03707,0.01709,
+    #  0.02789,0.03913,0.04424,0.04415,0.03183,0.05156,0.05786,0.05571,
+    #  0.05636,0.05105)
+    #
+    #Waals.names <- c("Ar","CO2","CS2","CO","CCl4","Cl2","C2H6S","C2H5OH",
+    #  "C6H5F","CH3F","CH4","CH3OH","C5H12","C3H8","H2O","He",
+    #  "H2","HBr","HCl","H2S","Hg","Kr","NH3","Ne",
+    #  "NO","N2","NO2","N2O","O2","PH3","SiH4","SiF4","SO2","Xe")
+    #
+    # a <- Waals.a[gas == Waals.names]
+    # b <- Waals.b[gas == Waals.names]
+    # if (is.na(a)) stop(paste("do not have a and b values for the gas",gas))
 
-#  T in degrees Kelvin
-TK = 273.15 + T
-R = 0.082058
+    nam <- row.names(waalscoef)
+    a <- waalscoef[gas == nam, "a"]
+    b <- waalscoef[gas == nam, "b"]
+    if (is.na(a)) stop(paste("do not have a and b values for the gas", gas))
+  }
 
-#V=xRT/P ;    R=0.0821 liter-atm / mole-K
-if (a==0 & b==0) V=  x*R* TK / P else
-{
-V <- NULL
-for (TT in TK) {for (PP in P) {for (xx in x)
- V = c(V,uniroot(fun<- function (V)((PP+xx*xx*a/(V^2))*(V/xx-b)-R*TT),c(-10,1e6))$root)
- }}
-}
-return(V)  # volume, liter
+  #  T in degrees Kelvin
+  TK = 273.15 + T
+  R = 0.082058
+
+  #V=xRT/P ;    R=0.0821 liter-atm / mole-K
+  if (a==0 & b==0) {
+    V =  x * R * TK / P
+  } else {
+    V <- NULL
+    for (TT in TK) {
+      for (PP in P) {
+        for (xx in x)
+          # V = c(V, uniroot(fun<- function (V)((PP+xx*xx*a/(V^2))*(V/xx-b)-R*TT),c(-10,1e6))$root)
+          V <- c(V,
+            uniroot(function (V) ((PP + xx * xx * a/(V^2)) *
+              (V/xx - b) - R * TT), c(-10, 1e6))$root)
+        }
+      }
+     }
+  return(V)  # volume, liter
 }
 
 # The reverse: liter to mol
@@ -69,7 +80,7 @@ l2mol <- function(x=1, # litre of the gas
                   gas=NULL,
                   a=0, # dm^6*bar/mol^2
                   b=0) # dm3/mol
-x/mol2l(1,T=T,P=P,a=a,b=b,gas=gas)     # mole of gas
+  x/mol2l(1, T=T, P=P, a=a, b=b, gas=gas)     # mole of gas
 
 
 
--- pkg/marelac/R/mol.weight.R
+++ pkg/marelac/R/mol.weight.R
@@ -1,8 +1,7 @@
 ##########################################################################
 # molar weight
+# Original version of K. Soetaert, limited to a small set of species
 ##########################################################################
-
-## ToDo: use the list of common species for speed-up
 
 #mol.weight <- function(species)
 ## converts from mol to g
@@ -40,31 +39,26 @@
 # converts from mol to g, works with vectors and simple lists
 # general parser for arbitrary species
 # chemical species: whose molecular weight should be estimated
+# (slower than the original limited version)
+# ToDo: ??? add speedup for certain common species
 ##########################################################################
 mol.weight <- function(species) {
   if (!is.vector(species)) stop("species must be a vector")
   if (!is.character(unlist(species))) stop("species must be character")
   molweight <- function(species) {
     with(AtomicWeight, {
-      trace = FALSE # for debugging, lines have to be removed in production versions
       # insert * before number (with one or more digits)
       s1 <- gsub("([0-9]+)", "*\\1+", species)
-      if (trace) cat(s1, "\n")
       # insert + after capital letters
       s1 <- gsub("([a-z,A-Z])", "\\1+", s1, perl=TRUE)
-      if (trace) cat(s1, "\n")
       # remove + before lower case letters
       s1 <- gsub("\\+([a-z])", "\\1", s1, perl=TRUE)
-      if (trace) cat(s1, "\n")
       # replace +* with only *
       s1 <- gsub("+*", "*", s1, fixed = TRUE)
-      if (trace) cat(s1, "\n")
       # remove trailing +
       s1 <- gsub("\\+$", "", s1)
-      if (trace) cat(s1, "\n")
       # remove trailing +)
       s1 <- gsub("\\+\\)", ")", s1)
-      if (trace) cat(s1, "\n")
       # calculate molar mass
       eval(parse(text=s1))
     })

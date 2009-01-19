0d508a73f96d87dce29536bae2b130d4df25a0ac
karlines (dr ) <karline.soetaert@nioz.nl>
2009-01-19 18:30:35 +0000

    fixed a newly appeared bug.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/marelac@39 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/marelac/R/satconc.R
+++ pkg/marelac/R/satconc.R
@@ -1,5 +1,5 @@
 satconc <- function(S = 35, T = 25, P = 1.013253, gas = "O2", atm = atmComp(gas))
 {
   if (is.null(atm)) atm <- atmComp(gas)
-  solubility(S = S, P = P, gas = gas) * P * atm * (1 - vapor(T = T, S = S))
+  solubility(S = S, T = T, P = P, gas = gas) * P * atm * (1 - vapor(T = T, S = S))
 }

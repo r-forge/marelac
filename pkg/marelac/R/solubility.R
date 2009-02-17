7d8ef3d2933840264eff4a658489669cb611262d
karlines (dr ) <karline.soetaert@nioz.nl>
2009-02-17 15:44:15 +0000

    wishlist.txt
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/marelac@42 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/marelac/R/solubility.R
+++ pkg/marelac/R/solubility.R
@@ -44,8 +44,8 @@ gas_solubility <- function (S = 35, t = 25, P = 1.013253, x = c("He","Ne",
   bet <- Sb$A1+Sb$A2*(100/K)+Sb$A3*log(K/100)+Sb$A4*(K/100)^2+S*(
          Sb$B1+Sb$B2*K/100+Sb$B3*(K/100)^2)
 
-  if (Sb$type==1) SA  <- exp(bet)/22.4136*10^6  else           #mmol/m3/bar
-                  SA  <- exp(bet)/P/(1-vapor(t=t,S=S))*10^6    #mmol/m3/bar
+  if (Sb$type==1) SA  <- exp(bet)/22.4136*10^6  else           #mmol/m3/atm
+                  SA  <- exp(bet)/P/(1-vapor(t=t,S=S))*10^6    #mmol/m3/atm
 
   SA
 

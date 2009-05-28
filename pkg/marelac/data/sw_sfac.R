cbcb77415b45ffbaa10b919c169bf27ae8dc721e
petzoldt (Dr ) <thomas.petzoldt@tu-dresden.de>
2009-05-28 19:36:06 +0000

    minors
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/marelac@75 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/marelac/data/sw_sfac.R
+++ pkg/marelac/data/sw_sfac.R
@@ -6,16 +6,22 @@
 # ndepth: depths of salinity correction ndepth[long,lat]
 # del_sa:salinity correction del_sal[nz,ny,nx]
 
-dsal <- read.table("gsw_data.dat")
-dsal[dsal=="NaN"]<- -99
 
-sw_sfac <- list()
-sw_sfac$longs <- dsal[1:91,1]
-sw_sfac$lats  <- dsal[92:(92+43),1]
-is <- 92+44
-sw_sfac$p  <- dsal[is:(is+44),]
-is <- 92+44+45
-sw_sfac$ndepth <- matrix(nc=91,nr=44,dsal[is:(is+(91*44)-1),1])
-ii <- is+(91*44)
-sw_sfac$del_sa <- array(dim=c(45,44,91),data=dsal[ii:(ii+45*44*91-1),1])
-rm(dsal,is,ii)
+sw_sfac <- local({
+
+  dsal <- read.table("gsw_data.dat")
+  dsal[dsal=="NaN"] <- -99
+
+  is1 <- 92 + 44
+  is2 <- 92 + 44 + 45
+  ii <- is2 + 91 * 44
+
+  list(
+    longs  = dsal[1:91, 1],
+    lats   = dsal[92:(92+43), 1],
+    p      = dsal[is1:(is1+44),],
+    ndepth = matrix(nc=91, nr=44, dsal[is2:(is2+(91*44)-1), 1]),
+    del_sa = array(dim=c(45, 44, 91), data=dsal[ii:(ii+45*44*91-1), 1])
+  )
+})
+#rm(dsal, is1, is2, ii)

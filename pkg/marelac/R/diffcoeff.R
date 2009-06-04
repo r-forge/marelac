97c174ace9e9b78e3029060fa2a41d705effbe4a
karlines (dr ) <karline.soetaert@nioz.nl>
2009-06-04 13:05:05 +0000

    checked UNESCO implementation - added some check values to help file. diffcoeff !!
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/marelac@91 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/marelac/R/diffcoeff.R
+++ pkg/marelac/R/diffcoeff.R
@@ -2,6 +2,44 @@
 ## Molecular diffusion coefficients
 ## -----------------------------------------------------------------------------
 
+  # Boudreau 1997 - Arrhenius formulation
+  # H2, CH4, He, Ne, Kr, Xe, Rn: Jahne et al (1987)
+  # DMS: Saltzman et al. (1993)
+  # Ar: Ohsumi and Horibe (1984)
+  .marelac$ArrDat <- data.frame(
+    A=c(3338, 3047, 2000, 818, 1608, 7238, 6393, 9007,15877),
+    Ea=c(16.06,18.36,18.10,11.70,14.84,19.81,20.20,21.61,23.26)
+  )
+  rownames(.marelac$ArrDat) <- c("H2","CH4","DMS","He","Ne","Ar","Kr","Xe","Rn")
+
+
+
+  #  from Wilke and Chang (1955) as modified by Hayduk and Laudie (1974)
+  .marelac$WCDat <- matrix(nc=1, data=c(34.7,35.2,24.5,23.6,36.0,34.5,43.8))
+  rownames(.marelac$WCDat) <- c("N2","H2S","NH3","NO","N2O","CO","SO2")
+
+  # Boudreau (1997): linear functions of temp
+  .marelac$BDat <- data.frame(
+    m0=c(c(25.9,10.0,9.60,6.29,9.81,5.06,4.33,4.02,3.26,2.62,10.4,6.35,
+      4.82,5.99,4.88,4.66,10.3,9.50,54.4,4.43,6.06,6.06,10.3,7.82,
+      9.50,3.60,3.43,3.31,3.18,4.06,2.57,3.31,3.31,3.39,3.63,3.36,
+      3.69,4.46,3.91,3.31,2.79,2.95,2.78,2.71)),
+    m1=c(1.094,0.441,0.438,0.343,0.432,0.275,0.199,0.223,
+      0.177,0.143,0.273,0.280,0.266,0.307,0.232,0.252,
+      0.331,0.388,1.555,0.241,0.297,0.297,0.416,0.359,
+      0.413,0.179,0.144,0.150,0.155,0.176,0.140,0.152,
+      0.152,0.158,0.208,0.130,0.169,0.198,0.199,0.151,
+      0.172,0.131,0.136,0.120))
+  rownames(.marelac$BDat) <- c("OH","Br","Cl","F","I","HCO3","CO3","H2PO4",
+      "HPO4","PO4","HS","HSO3","SO3","HSO4","SO4","IO3","NO2","NO3",
+      "H","Li","Na","K","Cs","Ag","NH4","Ca","Mg","Fe","Mn","Ba","Be",
+      "Cd","Co","Cu","Hg","Ni","Sr","Pb","Ra","Zn","Al","Ce","La","Pu")
+
+
+## -----------------------------------------------------------------------------
+## Molecular diffusion coefficients
+## -----------------------------------------------------------------------------
+
 diffcoeff <- function(S=35,        # Salinity, ppt
                       t=25,        # Temperature, degrees C
                       P=1.013253,  # Pressure, bar
@@ -16,250 +54,147 @@ diffcoeff <- function(S=35,        # Salinity, ppt
       "Ba","Be","Cd","Co","Cu","Hg","Ni","Sr","Pb","Ra","Zn","Al","Ce","La","Pu",
       "H3PO4","BOH3","BOH4","H4SiO4")
       ) {
-  
-   
+
+
   species   <- match.arg(species, several.ok = TRUE)
 
+  diffc <- list()     # will have the result
+  diffArr<-diffChang<-diffBoud<-NULL
   TK  <- t + 273.15   # Temperature, Kelvin
   Patm <- 1.013253
 
   #  The viscosity in pure water at atmospheric pressure and sample temperature.
-  mu_0 <- viscosity(S=0,t=t,P=1.013253)
+  mu_0 <- viscosity(S=0,t=t,P=Patm)
 
   #  Diffusion coefficient of Water at S = 0, P = P=1.013253, t = t
-  #  Cohen MH and Turnbull D (1959). 
-  #  Krynicki K, Green CD and Sawyer DW (1978). 
-  
-  A <- 12.5e-09*exp(-5.22e-04*P)
-  B <- 925.0*exp(-2.6e-04*P)
-  T0 <- 95.0 + 2.61e-02*P
-  D_H2O <- A*sqrt(TK)*exp(-B/(TK-T0))
-  D_H2O <- D_H2O*viscosity(S=0,t,P)/mu_0
+  #  Cohen MH and Turnbull D (1959).
+  #  Krynicki K, Green CD and Sawyer DW (1978).
 
-  
-  #  Diffusion coefficient of O2 and CO2 
+  if ("H2O" %in% species) {
+    A <- 12.5e-09*exp(-5.22e-04*P)
+    B <- 925.0*exp(-2.6e-04*P)
+    T0 <- 95.0 + 2.61e-02*P
+    D_H2O <- A*sqrt(TK)*exp(-B/(TK-T0))
+    diffc$H2O <- D_H2O*viscosity(S=0,t,P)/mu_0
+  }
+
+  #  Diffusion coefficient of O2 and CO2
   #  Boudreau (1997)
-  A <- 0.2604
-  B <- 0.006383
-  D_O2 <- (A+B*(TK/mu_0))*1E-09
+  if ("O2" %in% species) {
+    A <- 0.2604
+    B <- 0.006383
+    diffc$O2 <- (A+B*(TK/mu_0))*1E-09
+  }
+  if ("CO2" %in% species) {
+    A <- 0.1954
+    B <- 0.005089
+    diffc$CO2 <- (A+B*(TK/mu_0))*1E-09
+  }
+  # H3PO4 : Least (1984) determined D(H3PO4) at 25 deg C and 0 S.
+  #         Assume that this value can be scaled by the Stokes-Einstein
+  #         relationship to any other temperature.
 
-  A <- 0.1954
-  B <- 0.005089
-  D_CO2 <- (A+B*(TK/mu_0))*1E-09
-  
+  if ("H3PO4" %in% species) {
+    D_H3PO4 <- 0.87e-09
+    tS      <- 25.0
+    SS      <- 0.0
+    mu_S   <- viscosity(SS,tS,Patm)
+    diffc$H3PO4 <- D_H3PO4*(mu_S/mu_0)*(TK/(tS + 273.15))
+  }
+  #  B(OH)3 : Mackin (1986) determined D(B(OH)3) at 25 deg C and
+  #           about 29.2 S.
+  #           Assume that this value can be scaled by the Stokes-Einstein
+  #           relationship to any other temperature.
+
+  if ("BOH3" %in% species) {
+    D_BOH3 <- 1.12e-09
+    tS     <- 25.0
+    SS     <- 29.2
+    mu_S   <- viscosity(SS,tS,Patm)
+    diffc$BOH3 = D_BOH3*(mu_S/mu_0)*(TK/(tS + 273.15))
+  }
+  #  B(OH)4 : No information on this species ! Boudreau and
+  #           Canfield (1988) assume it is 12.5% smaller than B(OH)3.
+  #
+  if ("BOH4" %in% species) {
+    if (is.null(diffc$BOH3)) {
+      D_BOH3 <- 1.12e-09
+      tS     <- 25.0
+      SS     <- 29.2
+      mu_S   <- viscosity(SS,tS,Patm)
+      D_BOH3 = D_BOH3*(mu_S/mu_0)*(TK/(tS + 273.15))
+    } else D_BOH3 <- diffc$BOH3
+    diffc$BOH4 <- 0.875*D_BOH3
+  }
+
+  #  H4SiO4 : Wollast and Garrels (1971) found D(H4SiO4) at 25 deg C
+  #           and 36.1 ppt S.
+  #           Assume that this value can be scaled by the Stokes-Einstein
+  #           relationship to any other temperature.
+  if ("H4SiO4" %in% species) {
+    D_H4SiO4 <- 1.0E-09
+    tS <- 25.0
+    SS <- 36.1
+    mu_S   <- viscosity(SS,tS,Patm)
+    diffc$H4SiO4 <- D_H4SiO4*(mu_S/mu_0)*(TK/(tS + 273.15))
+  }
   #  Other dissolved gases
   #  Boudreau (1997)
   #  TK <- 298.15
+  Arrhenius <- function (A) A[1]*exp(-(A[2]*1000)/(8.314472*TK))*1.0E-09
 
-  Arrhenius <- function (A,Ea) A*exp(-(Ea*1000)/(8.314472*TK))*1.0E-09
+  ii  <- .marelac$ArrDat[species[which(species%in%rownames(.marelac$ArrDat))],]
+  if (nrow(ii)>0) diffArr <- apply(ii,1,Arrhenius)
 
-  D_H2  <- Arrhenius(A= 3338, Ea =16.06)  # Jahne et al (1987)
-  D_CH4 <- Arrhenius(A= 3047, Ea =18.36)  # Jahne et al (1987)
-  D_DMS <- Arrhenius(A= 2000, Ea =18.10)  # Saltzman et al. (1993)
-
-  D_He  <- Arrhenius(A=  818, Ea =11.70)  # Jahne et al (1987)
-  D_Ne  <- Arrhenius(A= 1608, Ea =14.84)  # Jahne et al (1987)
-  D_Ar  <- Arrhenius(A= 7238, Ea =19.81)  # Ohsumi and Horibe (1984)
-  D_Kr  <- Arrhenius(A= 6393, Ea =20.20)  # Jahne et al (1987)
-  D_Xe  <- Arrhenius(A= 9007, Ea =21.61)  # Jahne et al (1987)
-  D_Rn  <- Arrhenius(A=15877, Ea =23.26)  # Jahne et al (1987)
- 
 
   #  Other dissolved gases
   #  from Wilke and Chang (1955) as modified by Hayduk and Laudie (1974)
 
   WilkeChang <- function (Vb) 4.72E-07*TK/(mu_0*Vb^0.6)*1E-04
 
-  D_N2 <- WilkeChang(Vb=34.7)
-  D_H2S <- WilkeChang(Vb=35.2)
-  D_NH3 <- WilkeChang(Vb=24.5)
-  D_NO <- WilkeChang(Vb=23.6)
-  D_N2O <- WilkeChang(Vb=36.0)
-  D_CO <- WilkeChang(Vb=34.5)
-  D_SO2 <- WilkeChang(Vb=43.8)
-  
-   
+  ii <- .marelac$WCDat[species[which(species%in%rownames(.marelac$WCDat))],]
+  if (length(ii)>0) diffChang<-apply(data.frame(ii),1,WilkeChang)
+
   #  The coefficients in pure water for the following species are
   #  calculated by linear functions of temperature (deg C)
   #  coefficients as in Boudreau (1997).
 
-  Boudreau <- function (m0,m1) (m0+m1*t)*1.0e-10
 
-  D_OH    <- Boudreau(25.9,1.094)
-  D_Br    <- Boudreau(10.0,0.441)
-  D_Cl    <- Boudreau(9.60,0.438)
-  D_F     <- Boudreau(6.29,0.343)
-  D_I     <- Boudreau(9.81,0.432)
+  Boudreau <- function (m) (m[1]+m[2]*t)*1.0e-10
+  ii <- .marelac$BDat[species[which(species%in%rownames(.marelac$BDat))],]
+  if (nrow(ii)>0) diffBoud<-apply(ii,1,Boudreau)
 
-  D_HCO3  <- Boudreau(5.06,0.275)
-  D_CO3   <- Boudreau(4.33,0.199)
+# KS -> ThP
 
-  D_H2PO4 <- Boudreau(4.02,0.223)
-  D_HPO4  <- Boudreau(3.26,0.177)
-  D_PO4   <- Boudreau(2.62,0.143)
-
-  D_HS    <- Boudreau(10.4,0.273)
-  D_HSO3  <- Boudreau(6.35,0.280)
-  D_SO3   <- Boudreau(4.82,0.266)
-  D_HSO4  <- Boudreau(5.99,0.307)
-  D_SO4   <- Boudreau(4.88,0.232)
-
-  D_IO3   <- Boudreau(4.66,0.252)
-  D_NO2   <- Boudreau(10.3,0.331)
-  D_NO3   <- Boudreau(9.50,0.388)
-
-  D_H     <- Boudreau(54.4,1.555)
-  D_Li    <- Boudreau(4.43,0.241)
-  D_Na    <- Boudreau(6.06,0.297)
-  D_K     <- Boudreau(6.06,0.297)
-  D_Cs    <- Boudreau(10.3,0.416)
-  D_Ag    <- Boudreau(7.82,0.359)
-  D_NH4   <- Boudreau(9.50,0.413)
-
-  D_Ca    <- Boudreau(3.60,0.179)
-  D_Mg    <- Boudreau(3.43,0.144)
-  D_Fe    <- Boudreau(3.31,0.150)
-  D_Mn    <- Boudreau(3.18,0.155)
-
-  D_Ba    <- Boudreau(4.06,0.176)
-  D_Be    <- Boudreau(2.57,0.140)
-  D_Cd    <- Boudreau(3.31,0.152)
-  D_Co    <- Boudreau(3.31,0.152)
-  D_Cu    <- Boudreau(3.39,0.158)
-  D_Hg    <- Boudreau(3.63,0.208)
-  D_Ni    <- Boudreau(3.36,0.130)
-  D_Sr    <- Boudreau(3.69,0.169)
-  D_Pb    <- Boudreau(4.46,0.198)
-  D_Ra    <- Boudreau(3.91,0.199)
-  D_Zn    <- Boudreau(3.31,0.151)
-
-  D_Al    <- Boudreau(2.79,0.172)
-  D_Ce    <- Boudreau(2.95,0.131)
-  D_La    <- Boudreau(2.78,0.136)
-  D_Pu    <- Boudreau(2.71,0.120)
-
-  # H3PO4 : Least (1984) determined D(H3PO4) at 25 deg C and 0 S.
-  #         Assume that this value can be scaled by the Stokes-Einstein
-  #         relationship to any other temperature.
-
-  D_H3PO4 <- 0.87e-09
-  tS      <- 25.0
-  SS      <- 0.0
-  mu_S   <- viscosity(SS,tS,Patm)
-  D_H3PO4 <- D_H3PO4*(mu_S/mu_0)*(TK/(tS + 273.15))
-
-  #  B(OH)3 : Mackin (1986) determined D(B(OH)3) at 25 deg C and
-  #           about 29.2 S.
-  #           Assume that this value can be scaled by the Stokes-Einstein
-  #           relationship to any other temperature.
-
-  D_BOH3 <- 1.12e-09
-  tS     <- 25.0
-  SS     <- 29.2
-  mu_S   <- viscosity(SS,tS,Patm)
-  D_BOH3 = D_BOH3*(mu_S/mu_0)*(TK/(tS + 273.15))
-
-  #  B(OH)4 : No information on this species ! Boudreau and
-  #           Canfield (1988) assume it is 12.5% smaller than B(OH)3.
-  #
-  D_BOH4 <- 0.875*D_BOH3
-
-  #  H4SiO4 : Wollast and Garrels (1971) found D(H4SiO4) at 25 deg C
-  #           and 36.1 ppt S.
-  #           Assume that this value can be scaled by the Stokes-Einstein
-  #           relationship to any other temperature.
-  D_H4SiO4 <- 1.0E-09
-  tS <- 25.0
-  SS <- 36.1
-  mu_S   <- viscosity(SS,tS,Patm)
-  D_H4SiO4 <- D_H4SiO4*(mu_S/mu_0)*(TK/(tS + 273.15))
-
-  # SALINITY AND PRESSURE CORRECTION
-  #  To correct for pressure and salinity, the Stokes-Einstein relationship 
-  #  is used. This is not quite accurate, but it is at least consistent.
+  # THomas, depending on whether these things have one or more rows,
+  # I have to use cbind() or c(); cbind with a NULL or N2 does not work so I
+  # have to check if it has a value  - not very nice coding
   
+  if (is.matrix(diffc) | is.matrix(diffChang) |
+      is.matrix(diffBoud) | is.matrix(diffArr)) {
+    Diffc <- NA
+    if (length(diffc) > 0) Diffc<-cbind(Diffc,as.data.frame(diffc))
+    if (! is.null(diffArr)) Diffc<-cbind(Diffc,diffArr)
+    if (! is.null(diffChang)) Diffc<-cbind(Diffc,data.frame(diffChang))
+    if (! is.null(diffBoud)) Diffc<-cbind(Diffc,diffBoud)
+    Diffc <- as.data.frame(Diffc)
+  } else Diffc <- data.frame(c(diffc,diffArr,diffChang,diffBoud))
+  
+  # SALINITY AND PRESSURE CORRECTION
+  #  To correct for pressure and salinity, the Stokes-Einstein relationship
+  #  is used. This is not quite accurate, but it is at least consistent.
+
   mu <- viscosity(S,t,P)   #  viscosity at sample conditions
   fac <- (mu_0/mu)
 
-  diffc <- data.frame(      
-#    "H2O","O2","CO2","H2","CH4","DMS",
-     H2O  = D_H2O*fac,        
-     O2  = D_O2*fac,        
-     CO2 = D_CO2*fac,
-     H2  = D_H2*fac,        
-     CH4 = D_CH4*fac,
-     DMS = D_DMS*fac,        
-#    "He","Ne","Ar","Kr","Xe","Rn",
-     He  = D_He*fac,        
-     Ne  = D_Ne*fac,        
-     Ar  = D_Ar*fac,        
-     Kr  = D_Kr*fac,        
-     Xe  = D_Xe*fac,        
-     Rn  = D_Rn*fac,        
-#    "N2","H2S","NH3","NO","N2O","CO","SO2",
-     N2  = D_N2*fac,        
-     H2S = D_H2S*fac,        
-     NH3 = D_NH3*fac,        
-     NO  = D_NO*fac,        
-     N2O = D_N2O*fac,        
-     CO  = D_CO*fac,        
-     SO2 = D_SO2*fac,        
-#    "OH","F","Cl","Br","I",
-     OH  = D_OH*fac,        
-     F  = D_F*fac,        
-     Cl  = D_Cl*fac,        
-     Br  = D_Br*fac,        
-     I  = D_I*fac,        
-#    "HCO3","CO3","H2PO4","HPO4","PO4",
-     HCO3  = D_HCO3*fac,
-     CO3   = D_CO3*fac,
-     H2PO4 = D_H2PO4*fac,
-     HPO4  = D_HPO4*fac,
-     PO4   = D_PO4*fac,
-#    "HS","HSO3","SO3","HSO4","SO4","IO3","NO2","NO3",
-     HS    = D_HS*fac,
-     HSO3  = D_HSO3*fac,
-     SO3   = D_SO3*fac,
-     HSO4  = D_HSO4*fac,
-     SO4   = D_SO4*fac,
-     IO3   = D_IO3*fac,
-     NO2   = D_NO2*fac,
-     NO3   = D_NO3*fac,
-#    "H","Li","Na","K","Cs","Ag","NH4",
-     H     = D_H*fac,
-     Li    = D_Li*fac,
-     Na    = D_Na*fac,
-     K     = D_K*fac,
-     Cs    = D_Cs*fac,
-     Ag    = D_Ag*fac,
-     NH4   = D_NH4*fac,
-#    "Ca","Mg","Fe","Mn",
-     Ca    = D_Ca*fac,
-     Mg    = D_Mg*fac,
-     Fe    = D_Fe*fac,
-     Mn    = D_Mn*fac,
-#    "Ba","Be","Cd","Co","Cu","Hg","Ni","Sr","Pb","Ra","Zn","Al","Ce","La","Pu",
-     Ba    = D_Ba*fac,
-     Be    = D_Be*fac,
-     Cd    = D_Cd*fac,
-     Co    = D_Co*fac,
-     Cu    = D_Cu*fac,
-     Hg    = D_Hg*fac,
-     Ni    = D_Ni*fac,
-     Sr    = D_Sr*fac,
-     Pb    = D_Pb*fac,
-     Ra    = D_Ra*fac,
-     Zn    = D_Zn*fac,
-     Al    = D_Al*fac,
-     Ce    = D_Ce*fac,
-     La    = D_La*fac,
-     Pu    = D_Pu*fac,
-#    "H3PO4","BOH3","B0H4","H4SiO4")
-     H3PO4 = D_H3PO4*fac,
-     BOH3 = D_BOH3*fac,
-     BOH4 = D_BOH4*fac,
-     H4SiO4 = D_H4SiO4*fac)
+  # Ks -> ThPe HERE AGAIN, if salinity is a vector, then fac is also a vector
+  # and I use a loop here !
+  if (nrow(Diffc)!=length(fac)) {
+    DD <- Diffc
+    for (i in 1:(length(fac)-1)) DD<-rbind(DD,Diffc)
+    Diffc <- DD
+  }
+  diffc <- Diffc*fac
 
-  return(diffc[species])
+  return(diffc[species])  # [species] to have same ordering as input
 }
-       

af6e8e577dc9a115adf868719bf3a7dfee90caed
petzoldt (Dr ) <thomas.petzoldt@tu-dresden.de>
2009-11-09 20:54:33 +0000

    removed obsolete menu functionality
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/marelac@121 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/marelacTeaching/R/zzz.R
+++ pkg/marelacTeaching/R/zzz.R
@@ -1,8 +1,4 @@
 
 .onLoad <- function(lib, pkg) {
   cat("marelacTeaching package loaded\n")
-  if (.Platform$GUI == "Rgui") addMarelacMenu() #else print("Marelac menue works only in RGui")
 }
-
-## this is called by:  detach(package:marelacTeaching)
-.Last.lib <- function(libpath) remMarelacMenu()

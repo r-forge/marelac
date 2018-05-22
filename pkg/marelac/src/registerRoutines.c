6196436b95b79a5ecd7f9965caf2cceb01a43fc8
karlines (dr ) <karline.soetaert@nioz.nl>
2018-05-22 19:01:27 +0000

    fixed: registernativeroutines and Recycling array of length 1 in vector-array arithmetic is deprecated.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/marelac@196 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/marelac/src/registerRoutines.c
@@ -0,0 +1,25 @@
+// RegisteringDynamic Symbols
+#include<R_ext/Rdynload.h>
+#ifndef R_R_H
+#  include <R.h>
+#endif
+
+/*
+  SUBROUTINE gsw_delta_sa(p0,longs0,lats0,longs,lats,p,ndepth,           &
+     &                         del_sa,delta)
+*/    
+
+
+void F77_NAME(gsw_delta_sa)(double*, double*,  double*, double*, double*, int*, double*, double*);
+     
+
+R_FortranMethodDef marelacfortranMethods[] = {
+ {"gsw_delta_sa",(DL_FUNC) &F77_SUB(gsw_delta_sa), 8},
+ {NULL, NULL, 0}
+};
+
+void R_init_OceanView(DllInfo *info) {
+  R_registerRoutines(info, NULL, NULL, marelacfortranMethods, NULL);
+  R_useDynamicSymbols(info, FALSE); // disable dynamic searching  
+}
+
